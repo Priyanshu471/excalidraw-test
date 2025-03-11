57ccb26385c4df17f643258659fd88f3ba0393a1
Shreyash <shreyashjain1996@gmail.com>
2025-03-11 13:01:41 +0530

    added some files

--- src/SlideEditor.tsx
+++ src/SlideEditor.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Excalidraw } from "@excalidraw/excalidraw";
-import "@excalidraw/excalidraw/index.css";
 import { Slide } from "./types";
 
 export interface Props {
